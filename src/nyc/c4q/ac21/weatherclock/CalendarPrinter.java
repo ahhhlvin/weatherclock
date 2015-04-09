1d259f0549ca2ef3a6f8c06362d03381b176afa3
ahhhlvin <alvkuang@gmail.com>
2015-04-08 22:22:48 -0400

    alvin's temp updates

--- src/nyc/c4q/ac21/weatherclock/CalendarPrinter.java
+++ src/nyc/c4q/ac21/weatherclock/CalendarPrinter.java
@@ -1,5 +1,6 @@
 package nyc.c4q.ac21.weatherclock;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CalendarPrinter
@@ -24,14 +25,18 @@ public class CalendarPrinter
      * @param date
      *   The date containing the month to print.
      */
-    public static void printMonthCalendar(Calendar date) {
+    public static ArrayList<String> printMonthCalendar(Calendar date) {
         // Extract year, month, and day for our date.
         int year = date.get(Calendar.YEAR);
         int month = date.get(Calendar.MONTH);
         int day = date.get(Calendar.DAY_OF_MONTH);
+        ArrayList<String> lines = new ArrayList<String>();
+        String list="";
 
         // First, show the month name and year.
-        System.out.println(DateTime.getMonthNames().get(month) + " " + year);
+        list+=(DateTime.getMonthNames().get(month) + " " + year+"\n");
+        lines.add(list);
+        list="";
 
         // Start the calendar on the first day of the month.
         Calendar cal = Calendar.getInstance();
@@ -42,35 +47,42 @@ public class CalendarPrinter
         int indent = cal.get(Calendar.DAY_OF_WEEK) - Calendar.SUNDAY;
         for (int i = 0; i < indent; ++i)
             // We use four spaces for each day, below.
-            System.out.print("    ");
+            list+=("    ");
 
         // Now print each day of the month.  Keep going until we hit the next month.
-        while (cal.get(Calendar.YEAR) == year && cal.get(Calendar.MONTH) == month) {
+        while (cal.get(Calendar.YEAR) == year && cal.get(Calendar.MONTH) == month)
+        {
             // Print the day of the month.  Make sure to always print exactly
             // two characters.
             int d = cal.get(Calendar.DAY_OF_MONTH);
-            if (d < 10)
-                System.out.print(' ');
-            System.out.print(d);
+            if(d < 10) list += (" ");
+            list += (String.valueOf(d));
 
-            if (d == day)
+            if(d == day)
                 // Today!  Mark the day.
-                System.out.print('*');
+                list += ("*");
             else
                 // Not today; leave some space.
-                System.out.print(' ');
+                list += (" ");
 
-            if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+            if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+            {
                 // Saturday: move on to the next line for the next date.
-                System.out.println();
+                list += "\n";
+                lines.add(list);
+                list="";
+            }
+
             else
                 // Other days: just leave an extra space.
-                System.out.print(' ');
+                list+=(" ");
 
             // On to the next day.
             cal = DateTime.getNextDay(cal);
         }
-        System.out.println();
+        list+=("\n");
+        lines.add(list);
+        return lines;
     }
 
 }
--- src/nyc/c4q/ac21/weatherclock/Main.java
+++ src/nyc/c4q/ac21/weatherclock/Main.java
@@ -1,8 +1,14 @@
 package nyc.c4q.ac21.weatherclock;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Calendar;
 
@@ -12,6 +18,8 @@ public class Main {
      * SAMPLE CODE: Returns sunset time for the current day.
      */
     public static Calendar getSunset() {
+
+
         URL url = HTTP.stringToURL("http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY");
         String doc = HTTP.get(url);
         JSONObject obj = (JSONObject) JSONValue.parse(doc);
@@ -22,13 +30,54 @@ public class Main {
         Long sunsetTimestamp = (Long) sys.get("sunset");
         if (sunsetTimestamp == null)
             return null;
-        return DateTime.fromTimestamp(sunsetTimestamp);
+        return (DateTime.fromTimestamp(sunsetTimestamp));
     }
+
+
+
+
+    //
+    //
+    //
+    //
+    //
+    public static Calendar getSunrise() {
+
+        URL url = HTTP.stringToURL("http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY");
+        String doc = HTTP.get(url);
+        JSONObject obj = (JSONObject) JSONValue.parse(doc);
+
+        JSONObject sys = (JSONObject) obj.get("sys");
+        if (sys == null)
+            return null;
+        Long sunriseTimestamp = (Long) sys.get("sunrise");
+        if (sunriseTimestamp == null)
+            return null;
+        return DateTime.fromTimestamp(sunriseTimestamp);
+    }
+    //
+    //
+    //
+    //
+    //
+
+
+
 
     /**
      * SAMPLE CODE: Displays a very primitive clock.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException
+    {
+
+        JSONParser parser = new JSONParser();
+        JSONObject weather_json_obj = (JSONObject)parser.parse(new FileReader("/Users/alvin2/Desktop/accesscode/weatherclock/json-files/weather.json"));
+
+        // FIXME: get way to pull API data from URL!!
+        URL webUrl = HTTP.stringToURL(
+                "http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY");
+
+
         // Find out the size of the terminal currently.
         final int numCols = TerminalSize.getNumColumns();
         final int numRows = TerminalSize.getNumLines();
@@ -56,6 +105,11 @@ public class Main {
 
         // Get sunset time for the current day.
         Calendar sunset = getSunset();
+
+
+        // Get sunrise time for the current day.
+        Calendar sunrise = getSunrise();
+
 
         int xPosition = 1 + numCols / 2 - 5;
         while (true) {
@@ -89,13 +143,92 @@ public class Main {
             terminal.setBackgroundColor(AnsiTerminal.Color.BLACK);
 
             // Write sunset time in dark yellow.
-            String sunsetTime = DateTime.formatTime(sunset, false);
+            String sunsetTime = DateTime.formatTime(sunset, false) + " PM";
             terminal.setTextColor(AnsiTerminal.Color.YELLOW, false);
             terminal.moveTo(9, xPosition - 2);
-            terminal.write("sunset at " + sunsetTime);
+            terminal.write("Sunset Time: " + sunsetTime);
+
+            //
+            //
+            //
+            // Write sunrise time in dark yellow.
+            String sunriseTime = DateTime.formatTime(sunrise, false) + " AM";
+            terminal.setTextColor(AnsiTerminal.Color.YELLOW, false);
+            terminal.moveTo(10, xPosition - 2);
+            terminal.write("Sunrise Time: " + sunriseTime);
+            //
+            //
+            //
+            //
+
 
             // Pause for one second, and do it again.
-            DateTime.pause(1.0);
+
+            // FIXME: this changes the delay in displaying whatever comes after it!
+            //DateTime.pause(1.0);
+
+
+            int y = 12;
+            for (int i = 0; i < CalendarPrinter.printMonthCalendar(cal).size(); i++) {
+                terminal.moveTo(y,xPosition);
+                terminal.write(CalendarPrinter.printMonthCalendar(cal).get(i));
+                y++;
+            }
+
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            // Alvin's part! (TEMP STUFF!!)
+
+
+            JSONObject tempObj = (JSONObject) weather_json_obj.get("main");
+            JSONArray weatherArray = (JSONArray) weather_json_obj.get("weather");
+            JSONObject weatherHash = (JSONObject) weatherArray.get(0);
+
+
+
+
+            terminal.moveTo(19, xPosition);
+            terminal.setTextColor(AnsiTerminal.Color.WHITE, true);
+            terminal.write("---- CURRENT WEATHER INFO ----");
+
+            Double temperature = (Double)tempObj.get("temp");
+            temperature = ((temperature  - 273.15) * 1.8000 + 32);
+            terminal.moveTo(21, xPosition);
+            terminal.write("Temperature is currently: " + temperature + " °F");
+
+            Double pressure = (Double) tempObj.get("pressure");
+            pressure = pressure * 0.02952998751;
+            terminal.moveTo(23 ,xPosition);
+            terminal.write("Air Pressure is currently: " + pressure + " inHG");
+
+            Long humidity = (Long) tempObj.get("humidity");
+            terminal.moveTo(25 ,xPosition);
+            terminal.write("Humidity is currently: " + humidity + " %");
+
+
+            // ASCII ART FOR WEATHER SHOULD GO HERE...
+
+            String weather = (String) weatherHash.get("main");
+            terminal.moveTo(277 ,xPosition);
+            terminal.write("Current weather: " + weather);
+
+            String weatherDescrip = (String) weatherHash.get("description");
+            terminal.moveTo(31 ,xPosition);
+            terminal.write("Sky description: " + weatherDescrip);
+
+
+
+
         }
     }
 }
